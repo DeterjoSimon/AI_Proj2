c9c13463580e1c8c8fefe53def830dbc9831a321
Simon Moe Sørensen <moe.simon@gmail.com>
2020-05-11 21:36:05 +0200

    Added working CNF function for simple cases

--- cnf.py
+++ cnf.py
@@ -1,2 +1,103 @@
-def cnf(belief):
-    return
+from logic_operators import *
+
+
+def to_cnf(formula):
+    res1 = to_cnf1(formula)
+    print(res1)
+    res2 = to_cnf2(res1)
+    print(res2)
+    res3 = cnf_to_clauses(res2)
+    print(res3)
+    return res3
+
+
+def to_cnf1(formula):
+    # Eliminate Biconditionals
+    if isinstance(formula, Biconditional):
+        lhs = formula.formulas[0]
+        rhs = formula.formulas[1]
+        return to_cnf1(And(Implication(lhs, rhs), Implication(rhs, lhs)))
+
+    # Eliminate Implications
+    elif isinstance(formula, Implication):
+        lhs = formula.formulas[0]
+        rhs = formula.formulas[1]
+        return to_cnf1(Or(Not(lhs), rhs))
+
+    # Move negation inwards, using double negation and De Morgan rules
+    elif isinstance(formula, Not):
+        val = formula.formulas[0]
+
+        # Singular operations
+        if isinstance(val, Proposition):
+            return formula
+
+        elif isinstance(val, Not):
+            return val.formulas[0]
+
+        # Two sided operations
+        elif isinstance(val, And):
+            lhs = val.formulas[0]
+            rhs = val.formulas[1]
+            return to_cnf1(Or(Not(lhs), Not(rhs)))
+
+        elif isinstance(val, Or):
+            lhs = val.formulas[0]
+            rhs = val.formulas[1]
+            return to_cnf1(And(Not(lhs), Not(rhs)))
+
+        elif isinstance(val, Implication):
+            lhs = val.formulas[0]
+            rhs = val.formulas[1]
+            return to_cnf1(Or(Not(lhs), rhs))
+
+        return to_cnf1(val)
+
+    elif isinstance(formula, Or):
+        lhs = formula.formulas[0]
+        rhs = formula.formulas[1]
+        return Or(to_cnf1(lhs), to_cnf1(rhs))
+
+    elif isinstance(formula, And):
+        lhs = formula.formulas[0]
+        rhs = formula.formulas[1]
+        return And(to_cnf1(lhs), to_cnf1(rhs))
+
+    elif isinstance(formula, Proposition):
+        return formula
+
+    # Special case for distributive law
+    # return formula
+
+
+def to_cnf2(formula):
+    # Apply distributivity law by distributing disjunctions over conjunctions
+    if isinstance(formula, Or):
+        lhs = formula.formulas[0]
+        rhs = formula.formulas[1]
+
+        if isinstance(lhs, And):
+            return to_cnf2(And(Or(lhs.formulas[0], rhs), Or(lhs.formulas[1], rhs)))
+
+        if isinstance(rhs, And):
+            return to_cnf2(And(Or(lhs, rhs.formulas[0]), Or(lhs, rhs.formulas[1])))
+
+        return Or(to_cnf2(lhs), to_cnf2(rhs))
+
+    elif isinstance(formula, And):
+        lhs = formula.formulas[0]
+        rhs = formula.formulas[1]
+        return And(to_cnf2(lhs), to_cnf2(rhs))
+
+    elif isinstance(formula, Proposition) or isinstance(formula, Not):
+        return formula
+    # return formula
+
+
+def cnf_to_clauses(cnf):
+    if isinstance(cnf, And):
+        lhs = cnf.formulas[0]
+        rhs = cnf.formulas[1]
+        return cnf_to_clauses(lhs).union(cnf_to_clauses(rhs))
+    else:
+        return {cnf}
--- tests/test_cnf.py
+++ tests/test_cnf.py
@@ -1,23 +1,41 @@
 from unittest import TestCase
 
-from cnf import cnf
+from cnf import to_cnf, cnf_to_clauses
 from logic_operators import *
 
 
 class TestCnf(TestCase):
-    s = Proposition("s")
     p = Proposition("p")
+    q = Proposition("q")
     r = Proposition("r")
 
-    def test_cnf(self):
+    def test_cnf_1(self):
         """
         Convert belief: Robert will pass the exam if and only if he will be prepared or lucky
         to CNF.
         """
-        r = self.r
         p = self.p
-        s = self.s
+        q = self.q
+        r = self.r
 
-        belief = Biconditional(r, Or(p, s))
+        belief = Biconditional(r, Or(p, q))
 
-        assert cnf(belief) == {Or(Or(Not(r), p), s), Or(Not(p), r), Or(Not(s), r)}
+        clauses = to_cnf(belief)
+        assert clauses == {Or(Or(Not(r), p), q), Or(Not(p), r), Or(Not(q), r)}
+
+    def test_cnf_2(self):
+        p = self.p
+        q = self.q
+        r = self.r
+
+        belief = Implication(And(p, q), Not(r))
+        clauses = to_cnf(belief)
+        assert clauses == {Or(Or(Not(p), Not(q)), Not(r))}
+
+    def test_cnf_3(self):
+        p = self.p
+        q = self.q
+        r = self.r
+        belief = Biconditional(And(p, q), Implication(Or(r, q), And(p, r)))
+        clauses = to_cnf(belief)
+        assert clauses == {Or(r, q), Or(Or(Not(p), Not(r)), q), Or(Or(r, Not(q)), Not(p))}
