6e5e7de1e1bde2cb6bc4870d2b17e4dfcd1588bc
Simon Moe Sørensen <moe.simon@gmail.com>
2020-05-11 13:35:31 +0200

    Added test template for cnf

---
+++ cnf.py
@@ -0,0 +1,2 @@
+def cnf(belief):
+    return
---
+++ tests/test_cnf.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from Belief_base.belief_base import BeliefBase
+from cnf import cnf
+from logic_operators import *
+
+
+class TestCnf(TestCase):
+    s = Proposition("s")
+    p = Proposition("p")
+    r = Proposition("r")
+
+    def test_cnf(self):
+        """
+        Convert belief: Robert will pass the exam if and only if he will be prepared or lucky
+        to CNF.
+        """
+        r = self.r
+        p = self.p
+        s = self.s
+
+        belief = Biconditional(r, Or(p, s))
+
+        assert cnf(belief) == {Or(Or(Not(r), p), s), Or(Not(p), r), Or(Not(s), r)}
